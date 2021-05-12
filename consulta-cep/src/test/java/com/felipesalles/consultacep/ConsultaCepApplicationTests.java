d57a929bb91cad5327dfb582d4662a3eecb74a91
Felipe Salles <prof.felipe.salles@gmail.com>
2021-05-12 02:07:35 -0300

    first commit

---
+++ consulta-cep/src/test/java/com/felipesalles/consultacep/ConsultaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesalles.consultacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
